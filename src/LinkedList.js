a48d18266703d7623ff6bbbcc65d0951a6cf218d
fara0000 <fexri_imanzade@mail.ru>
2020-03-10 10:54:50 +0400

    ...

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -12,7 +12,7 @@ LinkedList.prototype.addElmAtStart = function(value) { //unshift
     this.size += 1;
 }
 
-LinkedList.prototype.init = function(array) {
+LinkedList.prototype.init = function(array) { //init
     for(i = 0; i < array.length; i++){
         this.addElmAtStart(array[i]);
     }
@@ -29,24 +29,70 @@ LinkedList.prototype.toArray = function() { //toArray
     return array;
 }
 
-LinkedList.prototype.clear = function(){
+LinkedList.prototype.clear = function() { //clear
     this.root = null;
     this.size = 0;
 }
 
-LinkedList.prototype.toString = function(){
-    
+LinkedList.prototype.toString = function() { //toString
+    let getString = "' ";
+    let tempNode = this.root;
+    while(tempNode){
+        getString += tempNode.value + ', ';
+        tempNode = tempNode.next;
+    }
+    getString += "'"
+    return getString ;
 }
 
+LinkedList.prototype.getListSize = function() { //getSize
+    let result = 0;
+    let tempNode = this.root;
+    while(tempNode){
+        tempNode = tempNode.next;
+        result++;
+    }
+    return result;
+}
 
+LinkedList.prototype.deleteFirstElm = function() { // shift
+    let FirstElm = this.root.value;
+    let tempNode = this.root.next;
+    this.root = tempNode;
+    this.size -= 1
+    return FirstElm;
+}
 
-const testList = new LinkedList();
-testList.init([1,2,3,'fakhri']);
-console.log(testList);
+LinkedList.prototype.deleteLastElm = function() { //pop
+    let result = null;
+    let previous = null;
+    let tempNode = this.root;
+    while(tempNode){
+        previous = tempNode;
+        tempNode = tempNode.next;
+        if (!tempNode.next){
+            result = tempNode.value;
+            previous.next = null;
+            this.size -= 1;
+            return result;
+            
+        }
+    }
+}
 
-console.log(testList.toArray());
-// testList.addElmAtStart(5);
-// console.log(testList);
+LinkedList.prototype.addElmAtEnd = function(value) { //push
+    let newNode = new LinkedNode(value);
+    let tempNode = this.root;
+    while(tempNode){
+        tempNode = tempNode.next;
+        if (!tempNode.next){
+            tempNode.next = newNode;
+            this.size += 1;
+            return this.toArray()
+        }
+    }
+}
+
 
 
 
---
+++ src/test/index.tests.js
@@ -0,0 +1,9 @@
+const LinkedList = require('../src/LinkedList');
+describe('HW5_LinkedList_OOP', function() {
+    describe('Initilization', function() {
+        it('should be defined', function() {
+            assert.isDefined(LinkedList.init, 'function has been defined')
+        });
+        it()
+    });
+});
